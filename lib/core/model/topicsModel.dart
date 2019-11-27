f48f66ecb835ec43bd1fce259d939ecff688350f
aleckie <alexmaina79@gmail.com>
2019-11-27 04:20:53 +0300

    topics Model and Builder completed

--- lib/core/model/topicsModel.dart
+++ lib/core/model/topicsModel.dart
@@ -2,18 +2,24 @@ class Topics {
   final String topicName;
   final String id;
   final String content;
+  final String iconText;
+  final String description;
 
-  Topics({this.topicName, this.id, this.content});
+  Topics({this.topicName, this.id, this.content, this.iconText, this.description});
 
   Topics.fromMap(Map snapshot, String id)
       : id = id ?? '',
         topicName = snapshot['topicName'] ?? '',
-        content = snapshot['content'] ?? '';
+        content = snapshot['content'] ?? '',
+        iconText =snapshot['iconText']?? '',
+        description = snapshot['description'] ?? '';
 
   toJson() {
     return {
       'topicName': topicName,
       'content': content,
+      'iconText': iconText,
+      'description': description,
     };
   }
 }
--- lib/pages/topicBuilder.dart
+++ lib/pages/topicBuilder.dart
@@ -15,12 +15,24 @@ class TopicBuilder extends StatelessWidget {
           MaterialPageRoute(builder: (_)=> TopicDetails(topic: topic)));
           print('Tapped');
       },
-      child: Padding(
-        padding: EdgeInsets.all(2),
-        child: ListTile(
-          title: Container(child: Text('${topic.topicName}'),),
-          trailing: Icon(Icons.arrow_forward_ios),
+      child: Column(
+          children:<Widget>[
+          Padding(
+          padding: EdgeInsets.all(2),
+          child: ListTile(
+            leading: CircleAvatar(radius: 20,backgroundColor: Colors.pink.shade900,
+            child: Text('${topic.iconText}', style: TextStyle(fontWeight: FontWeight.w900),),
+            ),
+            title: Container(child: Text('${topic.topicName}.',style: TextStyle(fontWeight: FontWeight.w600),),),
+            subtitle: Container(child: Text('${topic.description}',style: TextStyle(fontWeight: FontWeight.w200),),),
+            trailing: IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: (){
+              Navigator.push(context, 
+              MaterialPageRoute(builder: (_)=> TopicDetails(topic: topic,)));
+            },),
+          )
         ),
+        Divider()
+        ]
       ),
       
     );
